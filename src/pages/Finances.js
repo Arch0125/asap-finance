0e5e33d45f1e048eb65405a66dfca556f8ab968f
ankush.ad050203@gmail.com <ankush.ad050203@gmail.com>
2022-12-03 14:09:03 +0530

    router protocol bridge/chain

--- src/pages/Finances.js
+++ src/pages/Finances.js
@@ -3,6 +3,7 @@ import { Sdk, MetaMaskWalletProvider } from 'etherspot';
 import PayReq from '../components/PayReq';
 import { useState } from 'react';
 import Pay from '../components/Pay';
+import RouterWidget from '../Widgets/RouterWidget';
 
 const Finances = () => {
 
@@ -13,7 +14,24 @@ const Finances = () => {
       <div className="flex flex-row w-[80%] h-[80%] bg-bgwhite border-4 border-black shadow-solid ">
         <div className="flex flex-col w-[70%] h-full p-14 border-r-4 border-black">
           {
-            selection == '1' ?<div className='flex flex-row w-full justify-between' > <PayReq/></div> : <p>2</p>
+            selection == '1' ?<div className='flex flex-row w-full justify-between' > <PayReq/></div> : 
+            selection == '3' ? (
+              <div className="flex flex-row w-full justify-between">
+                <div className="px-[2px]">
+                  <p className="text-4xl font-bold mb-[10px]">Welcome to Router Protocol</p>
+                  <hr/>
+                  <p className="mt-[10px] text-2xl mb-[8px] font-semibold">A cross-chain protocol that enables</p>
+                  <p className="text-xl">▸ Fund Transfers</p>
+                  <p className="text-xl">▸ Asset Swaps</p>
+                  <p className="text-xl">▸ Cross-chain Apps</p>
+                </div>
+                <div>
+                  <RouterWidget />
+                </div>
+              </div>
+            ) : (
+              <p>2</p>
+            )
           }
         </div>
         <div className="flex flex-col w-[30%] h-full justify-center items-center">
@@ -22,8 +40,8 @@ const Finances = () => {
                 <p>0xankush</p>
                 <button onClick={()=>setSelection(1)} className="text-2xl font-extrabold self-start mt-6 bg-filler text-bgwhite border-4 border-black shadow-solid px-4 py-2 w-[250px] hover:bg-bgwhite hover:text-filler">Pay</button>
                 <button onClick={()=>setSelection(2)} className="text-2xl font-extrabold self-start mt-6 bg-filler text-bgwhite border-4 border-black shadow-solid px-4 py-2 w-[250px] hover:bg-bgwhite hover:text-filler">Lend/Borrow</button>
-                <button onClick={()=>setSelection(3)} className="text-2xl font-extrabold self-start mt-6 bg-filler text-bgwhite border-4 border-black shadow-solid px-4 py-2 w-[250px] hover:bg-bgwhite hover:text-filler">Swap</button>
-                <button onClick={()=>setSelection(4)} className="text-2xl font-extrabold self-start mt-6 bg-filler text-bgwhite border-4 border-black shadow-solid px-4 py-2 w-[250px] hover:bg-bgwhite hover:text-filler">Bridge</button>
+                <button onClick={()=>setSelection(3)} className="text-2xl font-extrabold self-start mt-6 bg-filler text-bgwhite border-4 border-black shadow-solid px-4 py-2 w-[250px] hover:bg-bgwhite hover:text-filler">Swap/Bridge</button>
+               
           </div>
         </div>
       </div>
