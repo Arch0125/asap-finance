0746205513c07a22fe82f0a47d14652b6a42d166
Arch0125 <dasarchisman25@gmail.com>
2022-12-03 16:42:58 +0530

    Update scw mobile

--- src/pages/Scwmobile.js
+++ src/pages/Scwmobile.js
@@ -15,13 +15,30 @@ const Scwmobile = () => {
     const createScwallet = async () => {
         const output = await sdk.computeContractAccount();
         setAddr(output.address);
+       
         console.log('contract account', output);
     }
-
+    
+    const sendtx = async () => {
+        // const sync = await sdk.syncAccount();
+        // console.log(sync);
+        await sdk.computeContractAccount();
+        console.log(sdk.state.accountAddress);
+        const output2 = await sdk.batchExecuteAccountTransaction({
+            to: '0xC5Db59D48700B6bC8D53cE773b21931d986DEa0E',
+            value: '0x016345785d8a0000',
+        });
+        console.log('batch execute account transaction', output2);
+        const output = await sdk.estimateGatewayBatch();
+        console.log('estimate gateway batch', output);
+        const submit = await sdk.submitGatewayBatch();
+        console.log('contract account', submit);
+    }
     return ( 
         <div className='flex flex-col w-screen h-screen justify-center items-center' >
             <button onClick={()=>createScwallet()} className='bg-filler text-bgwhite p-2 rounded-md font-bold text-xl'>Deploy SCW</button>
             <p className='text-xl text-textcolor p-2 font-bold'>{(addr.toString()).slice(0,7)}...{(addr.toString()).slice(37)}</p>
+            <button onClick={()=>sendtx()} className='bg-filler text-bgwhite p-2 rounded-md font-bold text-xl'>Send Tx</button>
         </div>
      );
 }
